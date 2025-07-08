d1d1cf23b45da048a1dffdecc5f59982d2239897
ivaylonikolov7 <ivnikolov721@gmail.com>
2025-07-08 14:05:31 +0300

    feat: Support shard realm for static files (#1009)
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

---
+++ examples/get_address_book.rs
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use clap::Parser;
+use hedera::{AccountId, Client, FileId, NodeAddressBookQuery, PrivateKey};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(long, env)]
+    operator_account_id: AccountId,
+
+    #[clap(long, env)]
+    operator_key: PrivateKey,
+
+    #[clap(long, env, default_value = "testnet")]
+    hedera_network: String,
+
+    #[clap(long, env, default_value_t = FileId::get_exchange_rates_file_id_for(0, 0))]
+    hedera_exchange_rates_file: FileId,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let _ = dotenvy::dotenv();
+    let args = Args::parse();
+
+    println!("Getting address book for shard 0, realm 0");
+    let client = Client::for_mirror_network_with_shard_realm(
+        vec!["testnet.mirrornode.hedera.com:443".to_string()],
+        0,
+        0,
+    )
+    .await?;
+
+    client.set_operator(args.operator_account_id, args.operator_key.clone());
+
+    println!("Getting address book for shard 0, realm 0");
+    let response = NodeAddressBookQuery::new()
+        .shard(0)
+        .realm(0)
+        .execute(&client)
+        .await?;
+
+    for node in response.node_addresses {
+        println!("{}", node.node_id);
+    }
+
+    Ok(())
+}
---
+++ tests/e2e/file/file_id.rs
@@ -0,0 +1,25 @@
+use hedera::FileId;
+
+#[tokio::test]
+async fn should_get_address_book_file_id_for_shard_realm() -> anyhow::Result<()> {
+    let file_id = FileId::get_address_book_file_id_for(1, 1);
+    assert_eq!(file_id.shard, 1);
+    assert_eq!(file_id.realm, 1);
+    Ok(())
+}
+
+#[tokio::test]
+async fn should_get_exchange_rates_file_id_for_shard_realm() -> anyhow::Result<()> {
+    let file_id = FileId::get_exchange_rates_file_id_for(1, 1);
+    assert_eq!(file_id.shard, 1);
+    assert_eq!(file_id.realm, 1);
+    Ok(())
+}
+
+#[tokio::test]
+async fn should_get_fee_schedule_file_id_for_shard_realm() -> anyhow::Result<()> {
+    let file_id = FileId::get_fee_schedule_file_id_for(1, 1);
+    assert_eq!(file_id.shard, 1);
+    assert_eq!(file_id.realm, 1);
+    Ok(())
+}
