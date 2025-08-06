c8e099f1da108e8224704dd4698d69ac57d98d55
gsstoykov <georgi.stoykov@limechain.tech>
2025-08-06 12:56:18 +0300

    fix: format
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

--- tests/e2e/topic/info.rs
+++ tests/e2e/topic/info.rs
@@ -106,7 +106,7 @@ async fn query_cost_small_max() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
-#[ignore]    
+#[ignore]
 async fn query_cost_insufficient_tx_fee() -> anyhow::Result<()> {
     let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
         return Ok(());
