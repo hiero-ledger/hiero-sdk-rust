de3874dee5b956cf7e79b3d32e48d928d189574b
gsstoykov <georgi.stoykov@limechain.tech>
2025-08-13 14:20:44 +0300

    fix: Format and ignore flaky tests
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

--- tests/e2e/batch_transaction.rs
+++ tests/e2e/batch_transaction.rs
@@ -1,6 +1,8 @@
 use std::str::FromStr;
+
 use hedera::{
     AccountCreateTransaction,
+    AccountId,
     BatchTransaction,
     Hbar,
     PrivateKey,
@@ -18,6 +20,7 @@ async fn can_execute_batch_transaction() -> anyhow::Result<()> {
     };
 
     // Given
+    let operator_id = AccountId::new(0, 0, 2);
     let operator_key = PrivateKey::from_str(
         "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137"
     )?;
@@ -25,9 +28,7 @@ async fn can_execute_batch_transaction() -> anyhow::Result<()> {
     let account_key = PrivateKey::generate_ed25519();
 
     let mut inner_transaction = AccountCreateTransaction::new();
-    inner_transaction
-        .set_key_without_alias(account_key.public_key())
-        .initial_balance(Hbar::new(1));
+    inner_transaction.set_key_without_alias(account_key.public_key()).initial_balance(Hbar::new(1));
 
     // Use batchify to prepare the transaction
     inner_transaction.batchify(&client, operator_key.public_key().into())?;
@@ -35,6 +36,8 @@ async fn can_execute_batch_transaction() -> anyhow::Result<()> {
     // When / Then
     let mut batch_transaction = BatchTransaction::new();
     batch_transaction.add_inner_transaction(inner_transaction.into())?;
+
+    client.set_operator(operator_id, operator_key);
 
     let tx_response = batch_transaction.execute(&client).await?;
     let _tx_receipt = tx_response.get_receipt(&client).await?;
